0f98a89893663f55ba17245b109363b88158f8ae
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2024-09-03 10:59:22 +0200

    stop blinking in command mode

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -28,7 +28,10 @@ return {
       },
     },
   },
-
+  {
+    "folke/noice.nvim",
+    commit = "d9328ef",
+  },
   -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
   { import = "lazyvim.plugins.extras.lang.json" },
 
