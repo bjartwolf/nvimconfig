04faa03423e4fd19c138558a8c325036942332cc
Bjørn Einar Bjartnes <bjorn.einar.bjartnes@4subsea.com>
2024-10-31 13:45:44 +0100

    browsecommand

--- init.lua
+++ init.lua
@@ -1,18 +1,25 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-function BrowseKhronos(args)
+function LookupKhronos(args)
   local arg = args["args"]
   local urlWithQuery = string.format("https://registry.khronos.org/OpenGL-Refpages/es3.0/html/%s.xhtml", arg)
   local command = string.format(":Browse %s", urlWithQuery)
   vim.cmd(command)
 end
 
-vim.api.nvim_create_user_command("BrowseGLSL", BrowseKhronos, { nargs = 1 })
+function BrowseKhronos()
+  local urlWithQuery = "https://registry.khronos.org/OpenGL-Refpages/es3.0/"
+  local command = string.format(":Browse %s", urlWithQuery)
+  vim.cmd(command)
+end
+
+vim.api.nvim_create_user_command("LookupGLSL", LookupKhronos, { nargs = 1 })
+vim.api.nvim_create_user_command("GLSLBrowse", BrowseKhronos, {})
 
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "glsl",
   callback = function()
-    vim.bo.keywordprg = ":BrowseGLSL"
+    vim.bo.keywordprg = ":LookupGLSL"
   end,
 })
