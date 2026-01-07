10f3a3049d6874ff1c7e3a0eb3a29146d7da3576
Bjørn Einar Bjartnes <bjorn.einar.bjartnes@4subsea.com>
2026-01-07 13:32:37 +0100

    extras in the right place

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -34,7 +34,6 @@ return {
     commit = "d9328ef",
   },
   -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
-  { import = "lazyvim.plugins.extras.lang.json" },
 
   -- add any tools you want to have installed below
   {
